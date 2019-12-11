b2a2818a5e4c4cd032a9de216633afc44e45d6fe
Libor Gabaj <libor.gabaj@gmail.com>
2019-12-11 22:27:42 +0100

    Plugins development

---
+++ plugins/iotgate_system.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""Plugin module for processing SoC events, mostly system temperature
+ measurement.
+
+ """
+__version__ = '0.1.0'
+__status__ = 'Beta'
+__author__ = 'Libor Gabaj'
+__copyright__ = 'Copyright 2019, ' + __author__
+__credits__ = [__author__]
+__license__ = 'MIT'
+__maintainer__ = __author__
+__email__ = 'libor.gabaj@gmail.com'
+
+
+# Standard library modules
+import os
+
+# Custom library modules
+from gbj_sw import iot as modIot
+
+
+class device(modIot.Plugin):
+    """Plugin class."""
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def id(self):
+        name = os.path.splitext(__name__)[0]
+        id = name.split('_')[1]
+        return id
