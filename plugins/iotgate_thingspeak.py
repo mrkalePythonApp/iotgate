e887f04f888ee6b26aff63b721e96af7a0f500ce
Libor Gabaj <gabaj.libor@slsp.sk>
2020-01-09 17:12:46 +0100

    Plugin development

--- plugins/iotgate_thingspeak.py
+++ plugins/iotgate_thingspeak.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Plugin module representing the cloud service ThingSpeak.
 
-- The module receives data from several MQTT topic, collect it in its cache,
+- The module receives data from several MQTT topic, collect it in its buffer,
   and publish it at once into the cloud at allowed intervals.
 
 - The plugin receives commands
@@ -9,8 +9,9 @@
   - reset plugin
 
 - The plugin receives data
-  - SoC temperature in centigrades from plugin `server`
-
+  - SoC temperature in centigrades from plugin `server`.
+  - Activity status of the cooling fan from plugin 'sfan'. The plugin forwards
+    published fan status to the cloud.
 
  """
 __version__ = '0.1.0'
@@ -37,18 +38,18 @@ from gbj_sw import timer as modTimer
 class Device(modIot.Plugin):
     """Plugin class."""
 
-
     class Parameter(Enum):
         """Enumeration of plugin parameters for MQTT publishing topics."""
         PERIOD = 'period'
-    
+        CHANNEL_ID = 'channel'
+
     class Source(Enum):
         """Enumeration of plugins parameters for processing their
         MQTT messages.
-        
+
         """
         TEMPERATURE_SYSTEM_DID = 'server'
-        TEMPERATURE_SYSTEM_PARAMETER = 'TEMPERATURE' # Source enum name
+        COOLING_FAN_DID = 'sfan'
 
     class Timer(Enum):
         """Period parameters for publishing to cloud."""
@@ -67,25 +68,18 @@ class Device(modIot.Plugin):
         DEFAULT_HOST = 'mqtt.thingspeak.com'
         DEFAULT_PORT = 1883
         CLIENT_ID = socket.gethostname()
+        FAN_STATUS_PREFIX = 'FAN State: '
 
-    class Fields(Enum):
-        """Semantics of used cloud fields. Max. 8 allowed."""
+    class CloudBuffer(Enum):
+        """Semantics of used cloud status and fields. Max. 8 fields allowed."""
+        FAN_STATUS = 'status'
         TEMPERATURE_SERVER = 'field1'
 
     def __init__(self):
         super().__init__()
         self._logger = logging.getLogger(' '.join([__name__, __version__]))
-        # Cloud attributes
         self._cloudprm = {}
-        # self._clientid = None
-        # self._host = None
-        # self._port = None
-        # self._mqtt_api_key = None
-        # self._channel_id = None
-        # self._write_api_key = None
-        # Data
-        self._fields = {}
-        self._status = None
+        self._buffer = {}  # Buffer for cloud fields
         self._timer = modTimer.Timer(self.period,
                                      self._callback_timer_publish,
                                      name='ThingSpeak')
@@ -142,21 +136,8 @@ class Device(modIot.Plugin):
 # Cloud actions
 ###############################################################################
     def _setup_cloud(self) -> NoReturn:
-        """Define cloud management."""
+        """Define cloud management parameters."""
         section = self.CloudConfig.SECTION.value
-        # self._clientid = f'{socket.gethostname()}_{self.did}'
-        # self._host = self.config.option(self.CloudConfig.OPTION_HOST.value,
-        #                                 section,
-        #                                 self.CloudConfig.DEFAULT_HOST.value)
-        # self._port = self.config.option(self.CloudConfig.OPTION_PORT.value,
-        #                                 section,
-        #                                 self.CloudConfig.DEFAULT_PORT.value)
-        # self._mqtt_api_key = self.config.option(
-        #     self.CloudConfig.OPTION_MQTT_API_KEY.value, section)
-        # self._channel_id = self.config.option(
-        #     self.CloudConfig.OPTION_CHANNEL_ID.value, section)
-        # self._write_api_key = self.config.option(
-        #     self.CloudConfig.OPTION_WRITE_API_KEY.value, section)
         self._cloudprm[self.CloudConfig.CLIENT_ID.name] = \
             self.CloudConfig.CLIENT_ID.value + f'_{self.did}'
         self._cloudprm[self.CloudConfig.OPTION_HOST.name] = \
@@ -176,12 +157,69 @@ class Device(modIot.Plugin):
         self._cloudprm[self.CloudConfig.OPTION_WRITE_API_KEY.name] = \
             self.config.option(self.CloudConfig.OPTION_WRITE_API_KEY.value,
                                section)
+        # Add channel id to plugin status parameter
+        self.set_param(self._cloudprm[self.CloudConfig.OPTION_CHANNEL_ID.name],
+                       self.Parameter.CHANNEL_ID)
+        # Initialize buffer
+        for field in self.CloudBuffer:
+            self._buffer[field.value] = None
 
+    @property
+    def status_fan(self) -> str:
+        """Recent received fan status from MQTT broker."""
+        return self._buffer[self.CloudBuffer.FAN_STATUS.value]
 
-    def publish_fields(self):
-        """Publish values in cache."""
-        # for field in self.Fields:
-        #     message = f'{self._fields[field.value]}'
+    @status_fan.setter
+    def status_fan(self, status: modIot.Status) -> NoReturn:
+        """Remap and update fan status."""
+        status_new = \
+            f'{self.CloudConfig.FAN_STATUS_PREFIX.value}' \
+            f'{status.value}'
+        self._buffer[self.CloudBuffer.FAN_STATUS.value] = status_new
+
+    def publish_buffer(self) -> NoReturn:
+        """Publish values in buffer."""
+        # Construct message payload
+        items = []
+        for field in self.CloudBuffer:
+            field_name = field.value
+            field_value = self._buffer[field_name]
+            if field_value:
+                field_item = f'{field_name}={field_value}'
+                items.append(field_item)
+        payload = '&'.join(items)
+        # Publish payload
+        if payload:
+            channel = self._cloudprm[self.CloudConfig.OPTION_CHANNEL_ID.name]
+            items = [
+                'channels',
+                channel,
+                'publish',
+                self._cloudprm[self.CloudConfig.OPTION_WRITE_API_KEY.name],
+            ]
+            topic = self.Separator.TOPIC.value.join(items)
+            msg = f'Publishing to ThingSpeak'
+            try:
+                # mqttpublish.single(
+                #     topic,
+                #     payload=payload,
+                #     hostname=self._cloudprm[self.CloudConfig.OPTION_HOST.name],
+                #     port=self._cloudprm[self.CloudConfig.OPTION_PORT.name],
+                #     auth={
+                #         'username':
+                #             self._cloudprm[self.CloudConfig.CLIENT_ID.name],
+                #         'password':
+                #             self._cloudprm[self.CloudConfig.OPTION_MQTT_API_KEY.name],
+                #     }
+                # )
+                self._buffer[self.CloudBuffer.FAN_STATUS.value] = None
+                log = f'{msg}, {channel=}, message: {payload}'
+                self._logger.debug(log)
+            except Exception as errmsg:
+                log = f'Publishing to ThingSpeak failed with error {errmsg}'
+                self._logger.exception(log)
+        else:
+            self._logger.debug('Nothing published to ThingSpeak')
 
 ###############################################################################
 # General actions
@@ -197,7 +235,7 @@ class Device(modIot.Plugin):
 
     def _callback_timer_publish(self):
         """Publish data from cache."""
-        self.publish_fields()
+        self.publish_buffer()
 
     def process_own_command(self,
                             value: str,
@@ -221,11 +259,10 @@ class Device(modIot.Plugin):
                      measure: Optional[str],
                      device: modIot.Plugin) -> NoReturn:
         """Process data originating in other device."""
-        # Receive data from server
+        # Process data from plugin 'system'
         if device.did == self.Source.TEMPERATURE_SYSTEM_DID.value:
             # Process SoC temperature
-            device_param = self.Source.TEMPERATURE_SYSTEM_PARAMETER.value
-            if parameter == device.Parameter[device_param].value \
+            if parameter == device.Parameter.TEMPERATURE.value \
                     and measure == modIot.Measure.VALUE.value:
                 try:
                     temperature = float(value)
@@ -233,7 +270,31 @@ class Device(modIot.Plugin):
                     log = f'Ignored invalid temperature {value=}'
                     self._logger.warning(log)
                 else:
-                    self._fields[self.Fields.TEMPERATURE_SERVER.value] \
+                    self._buffer[self.CloudBuffer.TEMPERATURE_SERVER.value] \
                         = temperature
                     log = f'Received SoC {temperature=}'
                     self._logger.debug(log)
+
+    def process_status(self,
+                       value: str,
+                       parameter: Optional[str],
+                       measure: Optional[str],
+                       device: modIot.Plugin) -> NoReturn:
+        """Process status originating in other device."""
+        # Process status from 'fan'
+        if device.did == self.Source.COOLING_FAN_DID.value:
+            # Process SoC temperature published by plugin 'system'
+            if parameter == device.Parameter.ACTIVITY.value \
+                    and measure is None:
+                status = value.strip()
+                if status in [
+                        modIot.Status.ACTIVE.value,
+                        modIot.Status.IDLE.value,
+                        modIot.Status.UNKNOWN.value,
+                ]:
+                    self.status_fan = modIot.Status(status)
+                    log = f'Received {status=}'
+                    self._logger.debug(log)
+                else:
+                    log = f'Ignored uknown {status=}'
+                    self._logger.warning(log)
